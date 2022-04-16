cdb787ac86bf77fbaf540115d3dc7d596818918e
笑脸 <1003514310@qq.com>
2022-04-16 10:58:59 +0000

    update src/api/index.ts. 请求方式修改

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,24 @@
-import { User } from '@/api/types/sports';
-import request from '@/util/request/index'
+import { request } from '@/util/request/index';
 
-export const mi = () => {
-    return request<User>({
-        url: 'http://localhost:81/v1/sport/submit/mi',
-        method: 'post'
+export const mi = (data) => {
+    return request({
+        url: '/v1/sport/submit/mi',
+        method: 'post',
+        data
     })
 }
 
-export const xin = () => {
-    return request<User>({
-        url: 'http://localhost:81/v1/sport/submit/xin',
-        method: 'post'
+export const xin = (data) => {
+    return request({
+        url: '/v1/sport/submit/xin',
+        method: 'post',
+        data
     })
 }
-export const ydq = () => {
-    return request<User>({
-        url: 'http://localhost:81/v1/sport/submit/yqd',
-        method: 'post'
+export const ydq = (data) => {
+    return request({
+        url: '/v1/sport/submit/yqd',
+        method: 'post',
+        data
     })
 }
